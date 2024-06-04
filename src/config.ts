c4e89d00e9c9c93b1f02277d5d7ef37ab4d0c463
Ahwx <ahwx@ahwx.org>
2024-06-04 18:05:16 +0200

    feat(config.ts): define some defaults

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site.
+
+export const SITE_TITLE = "liv's town"
+export const SITE_DESCRIPTION = "liv to your fullest"
+export const HOMEPAGE_URL = "https://liv.town"
